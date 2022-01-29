e10619dbe5dd5721744cc266f378a28b7db0805e
jani-e <j.eriksson91@gmail.com>
2022-01-29 18:27:20 +0200

    viikko2_tehtava9

---
+++ viikko2/tehtavat9-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -0,0 +1,52 @@
+package ohtu.verkkokauppa;
+
+public class Kauppa {
+
+    //private Varasto varasto;
+    private VarastoIO varasto;
+    //private Pankki pankki;
+    private PankkiIO pankki;
+    private Ostoskori ostoskori;
+    //private Viitegeneraattori viitegeneraattori;
+    private ViitegeneraattoriIO viitegeneraattori;
+    private String kaupanTili;
+
+    public Kauppa() {
+        varasto = Varasto.getInstance();
+        pankki = Pankki.getInstance();
+        viitegeneraattori = Viitegeneraattori.getInstance();
+        kaupanTili = "33333-44455";
+    }
+    
+    public Kauppa(VarastoIO varastoIO, PankkiIO pankkiIO, ViitegeneraattoriIO viitegeneraattoriIO) {
+        this.varasto = varastoIO;
+        this.pankki = pankkiIO;
+        this.viitegeneraattori = viitegeneraattoriIO;
+        kaupanTili = "33333-44455";
+    }
+
+    public void aloitaAsiointi() {
+        ostoskori = new Ostoskori();
+    }
+
+    public void poistaKorista(int id) {
+        Tuote t = varasto.haeTuote(id); 
+        varasto.palautaVarastoon(t);
+    }
+
+    public void lisaaKoriin(int id) {
+        if (varasto.saldo(id)>0) {
+            Tuote t = varasto.haeTuote(id);             
+            ostoskori.lisaa(t);
+            varasto.otaVarastosta(t);
+        }
+    }
+
+    public boolean tilimaksu(String nimi, String tiliNumero) {
+        int viite = viitegeneraattori.uusi();
+        int summa = ostoskori.hinta();
+        
+        return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
+    }
+
+}
