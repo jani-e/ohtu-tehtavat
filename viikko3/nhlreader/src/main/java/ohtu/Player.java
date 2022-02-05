9edc576e502f00cc2f6e2e8eb4513ed6c38f7d10
jani-e <j.eriksson91@gmail.com>
2022-02-05 11:35:24 +0200

    viikko3, teht1 tehty

---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,75 @@
+
+package ohtu;
+
+public class Player {
+    private String name;
+    private String nationality;
+    private int assists;
+    private int goals;
+    private int penalties;
+    private String team;
+    private int games;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    
+    public String getNationality() {
+        return this.nationality;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " Team " + getTeam() + " Goals " + getGoals() +
+                " Assists " + getAssists();
+    }
+      
+}
