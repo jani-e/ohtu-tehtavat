958f8cf861fa3c2d03853c556852574eac78e3d5
jani-e <j.eriksson91@gmail.com>
2022-02-09 16:57:20 +0200

    koodin muutos: lisätty ostoksien määrän palautus sekä niiden hinta

--- viikko4/TddOstoskori/src/main/java/ohtu/Ostoskori.java
+++ viikko4/TddOstoskori/src/main/java/ohtu/Ostoskori.java
@@ -1,8 +1,15 @@
 package ohtu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ostoskori {
+    
+    private List<Ostos> ostokset;
+    
+    public Ostoskori() {
+        this.ostokset = new ArrayList<>();
+    }
  
     public int tavaroitaKorissa() {
         // kertoo korissa olevien tavaroiden lukumäärän
@@ -11,13 +18,16 @@ public class Ostoskori {
         // jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", 
         //   tulee metodin palauttaa 2   
 
-        return -1;
+        return this.ostokset.size();
     }
  
     public int hinta() {
         // kertoo korissa olevien tuotteiden yhteenlasketun hinnan
- 
-        return -1;
+        int hinta = 0;
+        for (Ostos ostos : this.ostokset) {
+            hinta += ostos.hinta();
+        }
+        return hinta;
     }
  
     public void lisaaTuote(Tuote lisattava) {
