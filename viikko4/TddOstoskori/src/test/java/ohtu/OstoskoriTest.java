24eced7d3ff2baf2713891593d605ae5c45c069f
jani-e <j.eriksson91@gmail.com>
2022-02-09 18:05:29 +0200

    lisätty testi: korin hinta on sama kuin tuotteen hinta. ei koodimuutoksia

--- viikko4/TddOstoskori/src/test/java/ohtu/OstoskoriTest.java
+++ viikko4/TddOstoskori/src/test/java/ohtu/OstoskoriTest.java
@@ -31,5 +31,13 @@ public class OstoskoriTest {
         
         assertEquals(1, kori.tavaroitaKorissa());
     }
+    
+    //step 3
+    @Test
+    public void yhdenTuotteenLisaamisenJalkeenKorissaOnSamanTuotteenHinta() {
+        Tuote maito = new Tuote("maito", 3);
+        kori.lisaaTuote(maito);
+        assertEquals(3, kori.hinta());
+    }
 
 }
